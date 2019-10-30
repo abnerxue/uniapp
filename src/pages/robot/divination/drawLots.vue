fb01e2d70d6c1a65ba430020fa57feb4f23e9bf1
ljq <942436003@qq.com>
2019-10-30 14:50:33 +0800

    招揽生意视频页面提交

--- src/pages/robot/divination/drawLots.vue
+++ src/pages/robot/divination/drawLots.vue
@@ -1,14 +1,17 @@
 <!--
  * 梁嘉琪
  * 8:35:24
- 抽签页面
+ 招揽生意视频页面
  -->
 <template>
   <div class="drawLots">
-    <p class="start" v-on:click="startDraw()">开始抽签</p>
+    <div class="box">
+      <video src="../../../static/img/video.mp4" autoplay loop v-on:click="startDraw()"></video>
+    </div>
+    <!-- <p class="start" v-on:click="startDraw()">开始抽签</p>
     <div class="buttonBox">
       <p v-on:click="backIndex()"></p>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -23,15 +26,23 @@
       startDraw () {
         this.$router.push('/pages/robot/divination/gyqq')
       },
-      backIndex () { // 放回主页
-        this.$router.push('/pages/robot/index')
-      }
     }
   }
 </script>
 
 <style>
   .drawLots {
+    width: 100%;
+    height: 100%;
+  }
+  .box {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+  .box video {
     width: 100%;
     height: 100%;
   }
